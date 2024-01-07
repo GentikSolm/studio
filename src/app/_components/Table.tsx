bde139a8a214672f78b91243d146e39c7f978c41
Josh Brown <josh@mjbrowns.com>
2024-01-07 15:21:51 -0600

    feat: resizable columns

--- src/app/_components/Table.tsx
+++ src/app/_components/Table.tsx
@@ -1,13 +1,16 @@
 "use client";
 import {
   SortingState,
+  Table as TableType,
   flexRender,
   getCoreRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import clsx from "clsx";
 import { ChevronsUpDownIcon, SortAsc, SortDesc } from "lucide-react";
-import { useState } from "react";
+import { memo, useMemo, useState } from "react";
+
 export const Table = ({
   columns,
   data,
@@ -19,13 +22,13 @@ export const Table = ({
   const table = useReactTable({
     columns: columns.map((c) => ({
       header: ({ column }) => {
-        const currentSort = column.getIsSorted()
-        let icon = < ChevronsUpDownIcon className="h-4 w-4" />
-        if(currentSort === 'asc') icon = <SortAsc className="h-4 w-4"/>
-        if(currentSort === 'desc') icon = <SortDesc className="h-4 w-4"/>
+        const currentSort = column.getIsSorted();
+        let icon = <ChevronsUpDownIcon className="h-4 w-4" />;
+        if (currentSort === "asc") icon = <SortAsc className="h-4 w-4" />;
+        if (currentSort === "desc") icon = <SortDesc className="h-4 w-4" />;
         return (
           <button
-            className="flex w-full justify-between"
+            className="flex w-full items-center justify-between"
             onClick={() => {
               const cur = column.getIsSorted();
               if (!cur || cur === "asc") {
@@ -54,22 +57,45 @@ export const Table = ({
     getCoreRowModel: getCoreRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    columnResizeMode: "onChange",
     state: {
       sorting,
     },
   });
-  //https://ka-table.com/ ??
+  const colSizeInfo = table.getState().columnSizingInfo;
+  const columnSizeVars = useMemo(() => {
+    const headers = table.getFlatHeaders();
+    const colSizes: { [key: string]: number } = {};
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i]!;
+      colSizes[`--header-${header.id}-size`] = header.getSize();
+      colSizes[`--col-${header.column.id}-size`] = header.column.getSize();
+    }
+    return colSizes;
+    // For perf. stop complaining
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [colSizeInfo]);
+
   return (
     <div>
-      <table className="font-mono text-sm">
-        <thead>
+      <div
+        className="font-mono"
+        style={{
+          ...columnSizeVars, //Define column sizes on the <table> element
+          width: table.getTotalSize(),
+        }}
+      >
+        <div>
           {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
+            <div key={headerGroup.id} className="flex">
               {headerGroup.headers.map((header) => {
                 return (
-                  <th
+                  <div
                     key={header.id}
-                    className="min-w-32 border border-gray-700 px-2.5 py-1.5 text-left font-normal text-gray-400"
+                    className="relative border border-gray-600 px-2 py-1.5"
+                    style={{
+                      width: `calc(var(--header-${header?.id}-size) * 1px)`,
+                    }}
                   >
                     {header.isPlaceholder
                       ? null
@@ -77,40 +103,58 @@ export const Table = ({
                           header.column.columnDef.header,
                           header.getContext(),
                         )}
-                  </th>
+
+                    <div
+                      onDoubleClick={() => header.column.resetSize()}
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                      className={clsx(
+                        "absolute -right-2 top-0 z-10 h-full w-2 cursor-col-resize touch-none select-none",
+                      )}
+                    />
+                  </div>
                 );
               })}
-            </tr>
+            </div>
           ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <tr key={row.id} data-state={row.getIsSelected() && "selected"}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td
-                      key={cell.id}
-                      className="min-w-32 border border-gray-700 px-2.5 py-1.5 text-left font-normal text-gray-200"
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+        </div>
+        {/* When resizing any column we will render this special memoized version of our table body */}
+        {table.getState().columnSizingInfo.isResizingColumn ? (
+          <MemoizedTableBody table={table} />
+        ) : (
+          <TableBody table={table} />
+        )}
+      </div>
     </div>
   );
 };
+
+function TableBody({ table }: { table: TableType<any> }) {
+  return (
+    <div>
+      {table.getRowModel().rows.map((row) => (
+        <div key={row.id} className="flex w-fit">
+          {row.getVisibleCells().map((cell) => (
+            <div
+              key={cell.id}
+              className="overflow-hidden truncate border border-gray-600 px-1.5 py-1.5 text-sm"
+              style={{
+                width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
+              }}
+            >
+              {cell.renderValue<any>() ?? (
+                <span className="text-gray-400">NULL</span>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+//special memoized wrapper for our table body that we will use during column resizing
+export const MemoizedTableBody = memo(
+  TableBody,
+  (prev, next) => prev.table.options.data === next.table.options.data,
+) as typeof TableBody;
